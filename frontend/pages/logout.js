5062b01af6556e0d45a83154a86b050ee74884a0
sawaleebabie <sawalee.25042541@gmail.com>
2021-04-29 00:10:18 +0700

    picture test Agains

--- frontend/pages/logout.js
+++ frontend/pages/logout.js
@@ -29,7 +29,7 @@ export default function Logout({ token }) {
             </Head>
             <div>
                 <Image
-                    src="/laundry.png"
+                    src="/facebook.png"
                     alt="Picture"
                     width={900}
                     height={200}
