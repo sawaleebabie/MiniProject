d44f92418d22ffea974c811c016495c1fa94b434
sawaleebabie <sawalee.25042541@gmail.com>
2021-04-29 00:24:00 +0700

    Test Check Agian

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -22,7 +22,7 @@ const about = () => {
             <h2>Contact Me</h2>
             <div className={styles.datacontainer}><div>
                 <Image
-                    src = "/facebook.png"
+                    src = "/facebook.PNG"
                     alt="Picture"
                     width={50}
                     height={50}
@@ -31,7 +31,7 @@ const about = () => {
             </div> 
             <div>
                 <Image
-                    src="/line.png"
+                    src="/line.PNG"
                     alt="Picture"
                     width={50}
                     height={50}
@@ -40,7 +40,7 @@ const about = () => {
             </div> 
             <div>
                 <Image
-                    src= "/call.png"
+                    src= "/call.PNG"
                     alt="Picture"
                     width={50}
                     height={50}
